09725a35852c865ae323bc6c277a4f1c8c1f0a8d
Bruno Passos <bruno.passos@outlook.com.br>
2021-07-20 18:04:19 -0300

    :smile: Fazendo o statement e acessando ele via cpf

--- src/index.js
+++ src/index.js
@@ -37,6 +37,9 @@ app.post("/account", (request, response) => {
 app.get("/statement/:cpf", (request, response) => {
     const { cpf } = request.params;
 
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statemante);
 })
 
 app.listen(3333);
