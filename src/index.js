166a2b343e1e1459a3d2080ecc66e221beefa44c
Bruno Passos <bruno.passos@outlook.com.br>
2021-07-21 15:57:34 -0300

    :smile: Criando a função de deposito bancario

--- src/index.js
+++ src/index.js
@@ -9,27 +9,19 @@ app.use(express.json());
 const customers = [];
 
 // Middleware
-function verifyIfExistsAccountCPF(resquest, response, next) {
+function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers;
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+    const customer = customers.find((customer) => customer.cpf === cpf);
 
     if(!customer) {
         return response.status(400).json({error: "Customer not found"});
     }
 
-    request.customer = customers;
+    request.customer = customer;
 
     return next ();
 }
-
-
-/* 
-    cpf - string
-    name - string
-    id - uuid
-    statement - []
-*/
 
 app.post("/account", (request, response) => {
     const { name, cpf } = request.body;
@@ -51,7 +43,13 @@ app.post("/account", (request, response) => {
 })
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
     return response.json(customer.statemante);
 })
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+
+});
 
 app.listen(3333);
